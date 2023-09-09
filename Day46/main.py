e1b4738bd3a3234c4611ded7910006cff31677db
hankhuang <huanghank2000@gmail.com>
2023-09-09 18:07:42 +0800

    Spotify playlist from Billboard top 100

--- Day46/main.py
+++ Day46/main.py
@@ -2,40 +2,25 @@ from bs4 import BeautifulSoup
 from spotipy.oauth2 import SpotifyOAuth
 import requests
 import spotipy
+import os
 
-client_ID = "ae41b55336cc479e89307ede3e3be670"
-client_secret = "eba3d7f64ce94cbd9e8cbeed59701ae0"
-scope = "playlist-modify-private"
+client_ID = os.environ['client_ID']
+client_secret = os.environ['client_secret']
+scope = "playlist-modify-private playlist-read-private playlist-modify-public"
 
-auth = SpotifyOAuth(
-    client_id=client_ID,
-    client_secret=client_secret,
-    redirect_uri='http://example.com',
-    scope=scope,
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        client_id=client_ID,
+        client_secret=client_secret,
+        redirect_uri='http://example.com',
+        scope=scope,
+        show_dialog=True,
+        cache_path="token.txt",
+    )
 )
 
-
-endpoint = "https://api.spotify.com/v1/users/11164799549/playlists"
-
-
-
-body = {
-    "name": "New Playlist",
-    "description": "New playlist for Billboard top 100",
-    "public": False
-}
-
-playlist_response = requests.post(url=endpoint, json=body)
-print(playlist_response.text)
-
-# sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-# results = sp.current_user_saved_tracks()
-# for idx, item in enumerate(results['items']):
-#     track = item['track']
-#     print(idx, track['artists'][0]['name'], " – ", track['name'])
-
-
-
+user_id = sp.current_user()["id"]
+# print(user_id)
 
 travel_date = input('what year you would like to travel to?Type the date in YYY-MM-DD format:')
 
@@ -50,3 +35,23 @@ for song in song_selector:
     songs.append(song.get_text().strip())
 #
 # print(songs)
+
+songs_url_list = []
+for song in songs:
+    # Search for the song on Spotify
+    results = sp.search(q=song, type='track', limit=1)
+
+    # Extract the Spotify URI if a match is found
+    if results['tracks']['total'] > 0:
+        spotify_url = results['tracks']['items'][0]['uri']
+        songs_url_list.append(spotify_url)
+
+# print(songs_url_list)
+
+my_play_list = sp.user_playlist_create(user_id,
+                                       f"{travel_date} Billboard 100",
+                                       public=True,
+                                       collaborative=False,
+                                       description='YYYY-MM-DD Billboard 100')
+
+sp.playlist_add_items(playlist_id=my_play_list['id'], items=songs_url_list)
