7eb6dd18e1872106009ca53110ced7aff26b2d2a
hankhuang <huanghank2000@gmail.com>
2023-09-10 21:13:49 +0800

    Higher lower game by Flask

--- Day54/hello.py
+++ Day54/hello.py
@@ -3,10 +3,37 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def bold(function):
+    def wrapper():
+        return "<b>" + function() + "</b>"
+    return wrapper
+
+
+def emphasis(function):
+    def wrapper():
+        return "<em>" + function() + "</em>"
+    return wrapper
+
+
+def underline(function):
+    def wrapper():
+        return "<u>" + function() + "</u>"
+    return wrapper
+
+
 @app.route("/")
+@bold
+@emphasis
+@underline
 def hello_world():
     return "<p>Hello, World!</p>"
 
 
+@app.route("/<name>")
+def greet(name):
+    return f"Hello {name}"
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
