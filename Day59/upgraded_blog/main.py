592e1ccf12b42b91b6e341b4e6294fd7c3d1b4ee
hankhuang <huanghank2000@gmail.com>
2023-09-19 11:58:28 +0800

    Contact message to email

--- Day59/upgraded_blog/main.py
+++ Day59/upgraded_blog/main.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
 
 app = Flask(__name__)
+
+my_email = "huanghank8906@gmail.com"
+password = "xwcczfmjyoudaave"
 
 url = "https://api.npoint.io/238651492acd348bbdea"
 all_post = requests.get(url=url).json()
@@ -17,9 +21,27 @@ def about_page():
     return render_template("about.html")
 
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact_page():
-    return render_template("contact.html")
+    if request.method == 'GET':
+        return render_template("contact.html")
+    else:
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        message = request.form['message']
+        success_message = 'Successfully send the message!'
+
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(from_addr=my_email,
+                                to_addrs="huanghank2000@myyahoo.com",
+                                msg="Subject:Contact message from Hank's Blog\n\n"
+                                    f"name:{name}\nemail:{email}\nphone:{phone}\nmessage:{message}")
+
+        print(f"{name}\n{email}\n{phone}\n{message}")
+        return render_template("contact.html",success_message=success_message)
 
 
 @app.route('/post/<num>')
